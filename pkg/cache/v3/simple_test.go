a1e101f9e57d4ff487b238cd299adb45a757cdc6
Valerian Roche <valerian.roche@datadoghq.com>
2025-12-02 22:44:48 -0500

    Support snapshot resources in linear cache

--- pkg/cache/v3/simple_test.go
+++ pkg/cache/v3/simple_test.go
@@ -122,7 +122,9 @@ func TestSnapshotCacheWithTTL(t *testing.T) {
 		for _, res := range resources {
 			snapRes = append(snapRes, srfrt(res))
 		}
-		snapshots = append(snapshots, types.NewTypeSnapshot(typeURL, fixture.version, snapRes))
+		snap, err := types.NewTypeSnapshot(typeURL, fixture.version, snapRes)
+		require.NoError(t, err)
+		snapshots = append(snapshots, snap)
 	}
 	snapshotWithTTL, err := types.NewSnapshotFromTypeSnapshots(fixture.version, snapshots)
 	require.NoError(t, err)
@@ -460,7 +462,9 @@ func TestSnapshotCreateWatchWithResourcePreviouslyNotRequested(t *testing.T) {
 		for _, res := range resources {
 			snapRes = append(snapRes, srfrt(res))
 		}
-		snapshots = append(snapshots, types.NewTypeSnapshot(typeURL, fixture.version, snapRes))
+		snap, err := types.NewTypeSnapshot(typeURL, fixture.version, snapRes)
+		require.NoError(t, err)
+		snapshots = append(snapshots, snap)
 	}
 	snapshot2, err := types.NewSnapshotFromTypeSnapshots(fixture.version, snapshots)
 	require.NoError(t, err)
@@ -578,7 +582,8 @@ func (s *singleResourceSnapshot) GetTypeSnapshot(typeURL string) types.TypeSnaps
 		Resource: s.resource,
 		TTL:      &ttl,
 	}
-	return types.NewTypeSnapshot(typeURL, s.version, []types.SnapshotResource{res})
+	snapshot, _ := types.NewTypeSnapshot(typeURL, s.version, []types.SnapshotResource{res})
+	return snapshot
 }
 
 // TestSnapshotSingleResourceFetch is a basic test to verify that simple
